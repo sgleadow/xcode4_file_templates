b95f16a1f666da690a07ad0be04c09ae2962b449
Stewart Gleadow <sgleadow@thoughtworks.com>
2011-05-10 09:59:23 +1000

    New Ruby install script. Removed test case header

--- GHUnitTestCase.xctemplate/GHTestCase/___FILEBASENAME___.h
+++
@@ -1,8 +0,0 @@
-
-#import <GHUnitIOS/GHUnit.h>
-
-@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaSubclass___ {
-   
-}
-
-@end
--- install_templates.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/Custom\ Templates
-cp -r *.xctemplate ~/Library/Developer/Xcode/Templates/File\ Templates/Custom\ Templates
