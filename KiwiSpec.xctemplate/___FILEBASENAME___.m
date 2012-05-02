9fddbaad6161d401e941a8bfa85e2cf24a9c687c
Stewart Gleadow <sgleadow@thoughtworks.com>
2012-05-02 16:49:13 +1000

    Added simple Kiwi spec file template

---
+++ KiwiSpec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,21 @@
+#import <Kiwi/Kiwi.h>
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+
+describe(@"something about ___FILEBASENAMEASIDENTIFIER___", ^{
+    
+    __block NSObject *item;
+    
+    beforeEach(^{
+        item = [[NSObject alloc] init];
+    });
+    
+    context(@"initial state", ^{
+        it(@"should have been created", ^{
+            [item shouldNotBeNil];
+        });
+    });
+    
+});
+
+SPEC_END
